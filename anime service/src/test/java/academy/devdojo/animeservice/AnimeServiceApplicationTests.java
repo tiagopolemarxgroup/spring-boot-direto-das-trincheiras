63722e919896fe378e9d030dd09a8bc879b42680
Tiago <tiago.polemarxgroup@gmail.com>
2025-01-15 14:03:46 -0300

    initial commit of the project

---
+++ anime service/src/test/java/academy/devdojo/animeservice/AnimeServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package academy.devdojo.animeservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnimeServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
